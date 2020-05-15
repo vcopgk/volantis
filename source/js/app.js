6ad8c30c1f50a71901d5f60803e3c0387898c1c8
枋柚梓 <me@szyink.com>
2020-05-15 18:07:16 +0800

    upd

--- source/js/app.js
+++ source/js/app.js
@@ -409,3 +409,14 @@ var customSearch;
 
 
 })(jQuery);
+
+/*Valine Admin*/
+if(window.location.hash){
+	var checkExist = setInterval(function() {
+	   if (typeof jQuery == 'undefined'){return;}
+	   if ($("#"+window.location.hash.split("#")[1]).length) {
+		  $('html, body').animate({scrollTop: $("#"+window.location.hash.split("#")[1]).offset().top-90}, 1000);
+		  clearInterval(checkExist);
+	   }
+	}, 100);
+}
