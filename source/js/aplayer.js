ade785d70fb6acd33b18f85cca4c515048be116f
枋柚梓 <me@szyink.com>
2020-08-01 11:17:15 +0800

    更新

--- source/js/aplayer.js
+++ source/js/aplayer.js
@@ -12,10 +12,10 @@ function checkAPlayer() {
 // 设置全局播放器所对应的 aplyer 对象
 function setAPlayerObject() {
 	if (APlayerController.id == undefined) return;
-	document.querySelectorAll('meting-js').forEach((item, index)=>{
+	document.querySelectorAll('meting-js').forEach((item, index) => {
 		if (item.meta.id == APlayerController.id) {
 			if (document.querySelectorAll('meting-js')[index].aplayer != undefined) {
- 				APlayerController.aplayer = document.querySelectorAll('meting-js')[index].aplayer;
+				APlayerController.aplayer = document.querySelectorAll('meting-js')[index].aplayer;
 				setAPlayerObserver();
 			}
 		}
@@ -27,9 +27,32 @@ function setAPlayerObserver() {
 	try {
 		APlayerController.aplayer.on('play', function (e) {
 			updateAPlayerControllerStatus();
+			var index = document.querySelector('meting-js').aplayer.list.index;
+			var title = document.querySelector('meting-js').aplayer.list.audios[index].title;
+			var artist = document.querySelector('meting-js').aplayer.list.audios[index].artist;
+			$.message({
+				title: '音乐通知',
+				message: '正在播放：' + title + ' - ' + artist,
+				type: 'success'
+			});
 		});
 		APlayerController.aplayer.on('pause', function (e) {
 			updateAPlayerControllerStatus();
+			var index = document.querySelector('meting-js').aplayer.list.index;
+			var title = document.querySelector('meting-js').aplayer.list.audios[index].title;
+			var artist = document.querySelector('meting-js').aplayer.list.audios[index].artist;
+			$.message({
+				title: '音乐通知',
+				message: '暂停播放：' + title + ' - ' + artist,
+				type: 'success'
+			});
+		});
+		APlayerController.aplayer.on('error', function (e) { // 音乐加载失败
+			$.message({
+				title: "音乐通知",
+				message: "音乐加载失败~ 潜在网络问题",
+				type: 'warning'
+			});
 		});
 		APlayerController.aplayer.on('volumechange', function (e) {
 			onUpdateAPlayerVolume();
@@ -38,6 +61,7 @@ function setAPlayerObserver() {
 		// 监听音量手势
 		APlayerController.volumeBarWrap = document.getElementsByClassName('nav volume')[0].children[0];
 		APlayerController.volumeBar = APlayerController.volumeBarWrap.children[0]
+
 		function updateAPlayerVolume(e) {
 			let percentage = ((e.clientX || e.changedTouches[0].clientX) - APlayerController.volumeBar.getBoundingClientRect().left) / APlayerController.volumeBar.clientWidth;
 			percentage = Math.max(percentage, 0);
@@ -45,20 +69,20 @@ function setAPlayerObserver() {
 			APlayerController.aplayer.volume(percentage);
 		}
 		const thumbMove = (e) => {
-				updateAPlayerVolume(e);
-	  };
-	  const thumbUp = (e) => {
-	      APlayerController.volumeBarWrap.classList.remove('aplayer-volume-bar-wrap-active');
-	      document.removeEventListener('mouseup', thumbUp);
-	      document.removeEventListener('mousemove', thumbMove);
-	      updateAPlayerVolume(e);
-	  };
+			updateAPlayerVolume(e);
+		};
+		const thumbUp = (e) => {
+			APlayerController.volumeBarWrap.classList.remove('aplayer-volume-bar-wrap-active');
+			document.removeEventListener('mouseup', thumbUp);
+			document.removeEventListener('mousemove', thumbMove);
+			updateAPlayerVolume(e);
+		};
 
-	  APlayerController.volumeBarWrap.addEventListener('mousedown', () => {
-	      APlayerController.volumeBarWrap.classList.add('aplayer-volume-bar-wrap-active');
-	      document.addEventListener('mousemove', thumbMove);
-	      document.addEventListener('mouseup', thumbUp);
-	  });
+		APlayerController.volumeBarWrap.addEventListener('mousedown', () => {
+			APlayerController.volumeBarWrap.classList.add('aplayer-volume-bar-wrap-active');
+			document.addEventListener('mousemove', thumbMove);
+			document.addEventListener('mouseup', thumbUp);
+		});
 
 
 		// 设置完监听就立即更新一次
@@ -66,7 +90,6 @@ function setAPlayerObserver() {
 		onUpdateAPlayerVolume();
 		APlayerController.observer = true;
 		console.log('APlayerController ready!');
-
 	} catch (error) {
 		delete APlayerController.observer;
 	}
@@ -86,6 +109,7 @@ function updateAPlayerControllerStatus() {
 		console.log(error);
 	}
 }
+
 function onUpdateAPlayerVolume() {
 	try {
 		APlayerController.volumeBar.children[0].style.width = APlayerController.aplayer.audio.volume * 100 + '%'
@@ -135,31 +159,66 @@ function aplayerVolume(percent) {
 		console.log(error);
 	}
 }
-// 调节音量 测试
-function aplayerVolumeToggle() {
-	// checkAPlayer();
-	// try {
-	// 	if (APlayerController.aplayer.audio.volume == 0) {
-	// 		aplayerVolume(0.7);
-	// 	} else {
-	// 		aplayerVolume(0);
-	// 	}
-	// } catch (error) {
-	// 	console.log(error);
-	// }
+
+// 自动播放音乐
+function autoPlayMusic() {
+	let autoPlayMusic = getCookie('autoPlayMusic') == null ? true : false;
+	setTimeout(function () {
+		if ($(window).width() > 500 && autoPlayMusic && APlayerController.autoPlay) {
+			let isplay = true;
+			$.message({
+				title: "音乐通知",
+				message: "即将自动播放，点击<a id='stopMusic' class='stopMusic fix-cursor-pointer'>停止播放</a>",
+				type: 'warning'
+			});
+			setTimeout(function () {
+				if (APlayerController.aplayer == undefined) {
+					checkAPlayer();
+				}
+				if (isplay) {
+					aplayerToggle();
+				}
+			}, 3500);
+
+			$("#stopMusic").click(function () {
+				isplay = false;
+				$(".c-message--close").click();
+				setTimeout(() => {
+					$.message({
+						title: "音乐通知",
+						time: 999999,
+						message: "是否永久关闭音乐自动播放?  <a id='dontPlayMusic' class='stopMusic fix-cursor-pointer'><b>确认</b></a>",
+						type: 'warning'
+					});
+					$("#dontPlayMusic").click(function () {
+						var expires = 7 * 24 * 60 * 60 * 1000;
+						setCookie("autoPlayMusic", 'false', expires, '/');
+						$(".c-message--close").click();
+						setTimeout(() => {
+							$.message({
+								title: '音乐通知',
+								time: 6000,
+								message: '关闭成功，未来一周内将不再提醒~',
+								type: 'success'
+							});
+						}, 1000);
+					});
+				}, 1000);
+			})
+		}
+	}, 1500);
 }
 
 (function ($) {
 	// 网速快
 	checkAPlayer();
 	// 网速一般
-	setTimeout(function(){
+	setTimeout(function () {
 		checkAPlayer();
 	}, 3000);
 	// 网速较慢
-	setTimeout(function(){
+	setTimeout(function () {
 		checkAPlayer();
 	}, 10000);
-
-
+	autoPlayMusic();
 })(jQuery);
