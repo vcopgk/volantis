d8d3bee90acdfc1dc86ce8685f5fbba46f8914a2
枋柚梓 <me@szyink.com>
2020-08-01 20:18:30 +0800

    Merge remote-tracking branch 'volantis-x/master' into master-theme

--- source/js/aplayer.js
+++ source/js/aplayer.js
@@ -42,6 +42,10 @@ function setAPlayerObserver() {
         APlayerController.player.on('volumechange', function (e) {
 			onUpdateAPlayerVolume();
 		});
+		APlayerController.player.on('loadstart', function (e) {
+			// 跳到下一曲时更新标题
+			updateTitle();
+		});
 		APlayerController.player.on('error', function (e) { // 音乐加载失败
 			var index = document.querySelector('meting-js').aplayer.list.index;
 			var title = document.querySelector('meting-js').aplayer.list.audios[index].title;
