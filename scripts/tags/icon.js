4db6dcea25730a6d10b5798139458a3209003c81
szyink <me@szyink.com>
2021-04-23 18:23:50 +0800

    调整一些样式

--- scripts/tags/icon.js
+++ scripts/tags/icon.js
@@ -5,4 +5,11 @@ function iconfont(args) {
   return `<svg class="iconfont" aria-hidden="true"><use xlink:href="#icon-${args}"></use></svg>`;
 }
 
+function emoji(args) {
+  args = args.join(' ').split(',');
+  return `<svg class="iconfont emoji" aria-hidden="true"><use xlink:href="#icon-${args}"></use></svg>`;
+}
+
 hexo.extend.tag.register('iconfont', iconfont)
+
+hexo.extend.tag.register('emoji', emoji)
